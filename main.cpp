8ab393bb51d4ce92d3caefb006dc275c7447df86
Simmo Saan <simmo.saan@gmail.com>
2014-02-20 19:10:29 +0200

    Variadic functions, unary operators, evaluation

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 #include <vector>
 #include <stack>
 #include <map>
+#include <utility>
 #include <algorithm>
+#include <numeric>
+#include <cmath>
 #include <functional>
 
 using namespace std;
@@ -12,33 +15,88 @@ struct Op
 {
 	bool right;
 	int prec;
-	//function<double(double, double)> func;
+	bool unary;
 };
 
-map<string, Op> ops;
+multimap<string, Op> ops;
 map<string, function<double(vector<double>)>> funcs;
 
-vector<string> infix2postfix(string in)
+vector<pair<string, int>> infix2postfix(string in)
 {
-	vector<string> out;
-	stack<string> s;
+	vector<pair<string, int>> out;
+	stack<pair<string, int>> s;
 
+	pair<string, int> lasttok;
 	for (string::const_iterator it = in.cbegin(); it != in.cend();)
 	{
-		static string spaces = " \t\r\n";
+		static const string spaces = " \t\r\n";
 		if (spaces.find(*it) != string::npos)
 		{
 			++it;
 			continue;
 		}
 
-		static string numbers = "0123456789.";
+		/*cout << string(it, in.cend()) << endl;
+		cout << lasttok.first << "/" << lasttok.second << endl;
+		if (!s.empty())
+			cout << s.top().first << "/" << s.top().second << endl;*/
+
+		static const string numbers = "0123456789.";
 		string::const_iterator it2;
 		for (it2 = it; it2 != in.cend() && numbers.find(*it2) != string::npos; ++it2);
 		if (it2 != it)
 		{
-			out.push_back(string(it, it2));
+			out.push_back(lasttok = make_pair(string(it, it2), -1));
 			it = it2;
+			continue;
+		}
+
+		bool unary = lasttok.first == "" || lasttok.first == "(" || lasttok.first == "," || ops.find(lasttok.first) != ops.end();
+		/*cout << unary << endl;
+		cout << endl;*/
+
+		decltype(ops)::iterator oit;
+		for (oit = ops.begin(); oit != ops.end(); ++oit)
+		{
+			if (equal(oit->first.begin(), oit->first.end(), it) && oit->second.unary == unary)
+			{
+				break;
+			}
+		}
+		if (oit != ops.end())
+		{
+			if (unary)
+			{
+				s.push(lasttok = make_pair(oit->first, 1));
+			}
+			else
+			{
+				while (!s.empty())
+				{
+					bool found = false;
+					int tprec;
+					auto range = ops.equal_range(s.top().first);
+					for (auto oit2 = range.first; oit2 != range.second; ++oit2)
+					{
+						if (s.top().second == (oit2->second.unary ? 1 : 2))
+						{
+							tprec = oit2->second.prec;
+							found = true;
+							break;
+						}
+					}
+
+					if (found && ((!oit->second.right && oit->second.prec == tprec) || (oit->second.prec < tprec)))
+					{
+						out.push_back(s.top());
+						s.pop();
+					}
+					else
+						break;
+				}
+				s.push(lasttok = make_pair(oit->first, 2));
+			}
+			it += oit->first.size();
 			continue;
 		}
 
@@ -52,57 +110,43 @@ vector<string> infix2postfix(string in)
 		}
 		if (fit != funcs.end())
 		{
-			s.push(fit->first);
+			s.push(lasttok = make_pair(fit->first, 0));
 			it += fit->first.size();
 			continue;
 		}
 
 		if (*it == ',')
 		{
+			s.top().second++;
+
 			bool found = false;
 			while (!s.empty())
 			{
-				string tok = s.top();
-				s.pop();
-				if (tok == "(")
+				auto tok = s.top();
+
+				if (tok.first == "(")
 				{
 					found = true;
 					break;
 				}
 				else
+				{
 					out.push_back(tok);
+					s.pop();
+				}
 			}
 
 			if (!found)
-				cout << "ERROR" << endl;
+				cout << "ERROR," << endl;
 
+			lasttok = make_pair(",", 0);
 			++it;
-			continue;
-		}
-
-		decltype(ops)::iterator oit;
-		for (oit = ops.begin(); oit != ops.end(); ++oit)
-		{
-			if (equal(oit->first.begin(), oit->first.end(), it))
-			{
-				break;
-			}
-		}
-		if (oit != ops.end())
-		{
-			while (!s.empty() && ops.find(s.top()) != ops.end() && ((!oit->second.right && oit->second.prec == ops[s.top()].prec) || (oit->second.prec < ops[s.top()].prec)))
-			{
-				out.push_back(s.top());
-				s.pop();
-			}
-			s.push(oit->first);
-			it += oit->first.size();
 			continue;
 		}
 
 		if (*it == '(')
 		{
-			s.push("(");
+			s.push(lasttok = make_pair("(", 1));
 			++it;
 			continue;
 		}
@@ -112,39 +156,46 @@ vector<string> infix2postfix(string in)
 			bool found = false;
 			while (!s.empty())
 			{
-				string tok = s.top();
-				s.pop();
-				if (tok == "(")
+				auto tok = s.top();
+				if (tok.first == "(")
 				{
 					found = true;
 					break;
 				}
 				else
+				{
 					out.push_back(tok);
+					s.pop();
+				}
 			}
 
 			if (!found)
-				cout << "ERROR" << endl;
+				cout << "ERROR)" << endl;
 
-			if (!s.empty() && funcs.find(s.top()) != funcs.end())
+			auto tok = s.top();
+			s.pop();
+
+			if (!s.empty() && ops.find(s.top().first) == ops.end() && funcs.find(s.top().first) != funcs.end())
 			{
-				out.push_back(s.top());
+				out.push_back(make_pair(s.top().first, tok.second));
 				s.pop();
 			}
 
+			lasttok = make_pair(")", 0);
 			++it;
 			continue;
 		}
 
+		lasttok = make_pair("", 0);
 		cout << "ERROR" << endl;
-		return vector<string>();
+		return vector<pair<string, int>>();
 	}
 
 	while (!s.empty())
 	{
-		string tok = s.top();
+		auto tok = s.top();
 		s.pop();
-		if (tok == "(")
+		if (tok.first == "(")
 			cout << "ERROR" << endl;
 		out.push_back(tok);
 	}
@@ -152,21 +203,87 @@ vector<string> infix2postfix(string in)
 	return out;
 }
 
+double evalpostfix(vector<pair<string, int>> p)
+{
+	stack<double> s;
+	for (auto &tok : p)
+	{
+		if (tok.second == -1)
+			s.push(stod(tok.first));
+		else
+		{
+			if (s.size() < tok.second)
+				cout << "ERRORN" << endl;
+			else
+			{
+				vector<double> v;
+				for (int i = 0; i < tok.second; i++)
+				{
+					v.insert(v.begin(), s.top());
+					s.pop();
+
+				}
+
+				s.push(funcs[tok.first](v));
+			}
+		}
+	}
+
+	if (s.size() == 1)
+		return s.top();
+	else
+		cout << "ERRORM" << endl;
+}
+
 int main()
 {
-	ops["+"] = Op{false, 1};
-	ops["-"] = Op{false, 1};
-	ops["*"] = Op{false, 2};
-	ops["/"] = Op{false, 2};
-	ops["^"] = Op{true, 3};
+	ops.insert(make_pair("+", Op{false, 1, false}));
+	ops.insert(make_pair("-", Op{false, 1, false}));
+	ops.insert(make_pair("*", Op{false, 2, false}));
+	ops.insert(make_pair("/", Op{false, 2, false}));
+	ops.insert(make_pair("^", Op{true, 3, false}));
+	ops.insert(make_pair("+", Op{false, 10, true}));
+	ops.insert(make_pair("-", Op{false, 10, true}));
+
+	funcs["+"] = [](vector<double> v)
+	{
+		if (v.size() == 1)
+			return v[0];
+		else
+			return v[0] + v[1];
+	};
+	funcs["-"] = [](vector<double> v)
+	{
+		if (v.size() == 1)
+			return -v[0];
+		else
+			return v[0] - v[1];
+	};
+	funcs["*"] = [](vector<double> v)
+	{
+		return v[0] * v[1];
+	};
+	funcs["/"] = [](vector<double> v)
+	{
+		return v[0] / v[1];
+	};
+	funcs["^"] = [](vector<double> v)
+	{
+		return pow(v[0], v[1]);
+	};
+	funcs["max"] = [](vector<double> v)
+	{
+		return *max_element(v.begin(), v.end());
+	};
 
 	string exp;
     while (cout << "> ", getline(cin, exp))
 	{
 		auto postfix = infix2postfix(exp);
 		for (auto &tok : postfix)
-			cout << tok << " ";
+			cout << tok.first << "/" << tok.second << " ";
 		cout << endl;
+		cout << evalpostfix(postfix) << endl;
 		cout << endl;
 	}
     return 0;
